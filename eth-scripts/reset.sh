1599356af6ba3a38de755309d959e4332b3111d7
zekaf <jose.faisca@gmail.com>
2016-04-02 00:24:03 +0100

    Update reset.sh

--- eth-scripts/reset.sh
+++ eth-scripts/reset.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-# reset ethereum
+# Reset Host 
 
 ETHEREUM_DIR=$HOME
-rm -ri $ETHEREUM_DIR/go-ethereum
-rm -rf $ETHEREUM_DIR/eth-net-intelligence-api
-rm -rf $ETHEREUM_DIR/.ethereum
-(exec "./regenerate_ssh-hostkeys.sh")
+
+read -p "Remove $ETHEREUM_DIR/go-ethereum? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  rm -rfv $ETHEREUM_DIR/go-ethereum
+else
+  echo "No"
+fi
+
+read -p "Remove $ETHEREUM_DIR/eth-net-intelligence-api? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  rm -rfv $ETHEREUM_DIR/eth-net-intelligence-api
+else
+  echo "No"
+fi
+
+read -p "Remove $ETHEREUM_DIR/.ethereum? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  rm -rIv $ETHEREUM_DIR/.ethereum
+else
+  echo "No"
+fi
+
+read -p "Regenerate Host Keys? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  (exec "./regenerate_ssh-hostkeys.sh")
+else
+  echo "No"
+fi
 
 

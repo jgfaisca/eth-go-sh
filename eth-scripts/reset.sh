bc2612ad573761b0b7b3e3ea5226de8f41b46567
zekaf <jose.faisca@gmail.com>
2016-04-01 23:41:20 +0100

    Update reset.sh

--- eth-scripts/reset.sh
+++ eth-scripts/reset.sh
@@ -4,7 +4,7 @@
 
 ETHEREUM_DIR=$HOME
 rm -ri $ETHEREUM_DIR/go-ethereum
-rm -ri $ETHEREUM_DIR/eth-net-intelligence-api
-rm -ri $ETHEREUM_DIR/.ethereum
+rm -rf $ETHEREUM_DIR/eth-net-intelligence-api
+rm -rf $ETHEREUM_DIR/.ethereum
 
 

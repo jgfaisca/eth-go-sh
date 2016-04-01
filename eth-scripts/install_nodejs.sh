6940d2441f651515f9c47e6861074eb23c735774
zekaf <jose.faisca@gmail.com>
2016-04-01 23:36:50 +0100

    Update install_nodejs.sh

--- eth-scripts/install_nodejs.sh
+++ eth-scripts/install_nodejs.sh
@@ -7,14 +7,24 @@
 #
 #
 
-# version
+# nodejs version
 VERSION="5"
 
-if ! curl --version &> /dev/null ; then
-  echo "curl is not being recognized as a command; installing...";
-  sudo apt-get -y install curl
+# install curl
+curl --version >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "curl is not being recognized as a command; installing... ";
+  sudo apt-get install -y curl
 fi
 
+# install node
 curl -sL https://deb.nodesource.com/setup_$VERSION.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# install build-essential
 sudo apt-get install -y build-essential
+
+# output version
+node --version
+
+exit 0

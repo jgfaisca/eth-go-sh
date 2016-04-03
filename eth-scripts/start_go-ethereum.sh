bb245b7b14eeb65bcd8630e9e6faa97a29f4a61b
zekaf <jose.faisca@gmail.com>
2016-04-03 16:49:24 +0100

    Update start_go-ethereum.sh

--- eth-scripts/start_go-ethereum.sh
+++ eth-scripts/start_go-ethereum.sh
@@ -8,7 +8,9 @@ pm2 start  $ETHEREUM_DIR/eth-net-intelligence-api/app.json
 echo "PM2 started"
 
 # start go-ethereum
-nohup $ETHEREUM_DIR/go-ethereum/build/bin/geth --rpc --maxpeers "25" --verbosity "0"&
+nohup $ETHEREUM_DIR/go-ethereum/build/bin/geth --rpc --maxpeers "25" --verbosity "0" \
+>$ETHEREUM_DIR/ethereum.log 2>$ETHEREUM_DIR/ethereum.err &
+
 echo "Ethereum started"
 
 

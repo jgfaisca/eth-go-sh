2d4b35a3591f2de4d41b636bcbafa792934053d7
zekaf <jose.faisca@gmail.com>
2016-04-04 11:48:40 +0100

    Update update_go-ethereum.sh

--- eth-scripts/update_go-ethereum.sh
+++ eth-scripts/update_go-ethereum.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# update go-ethereum
+#
 
 cd $HOME/go-ethereum
 git pull
@@ -6,7 +9,8 @@ export GO_OPENCL=true
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
 make geth
-$HOME/eth-scripts/restart_go-ethereum.sh
+$HOME/eth-scripts/stop_go-ethereum.sh
+$HOME/eth-scripts/start_go-ethereum.sh
 
 exit 0
 

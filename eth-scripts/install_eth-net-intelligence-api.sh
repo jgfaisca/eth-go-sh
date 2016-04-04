d3f4fb42a14199b9fae6c872ff0bbba204be4b4b
zekaf <jose.faisca@gmail.com>
2016-04-04 12:16:09 +0100

    Update install_eth-net-intelligence-api.sh

--- eth-scripts/install_eth-net-intelligence-api.sh
+++ eth-scripts/install_eth-net-intelligence-api.sh
@@ -3,11 +3,11 @@
 # install eth-net-intelligence-api
 #
 
-if [[ -d "$HOME/eth-net-intelligence-api" ]]; then
-    	echo "remove $HOME/eth-net-intelligence-api before install" 
+if [[ -d "$ETHNETPATH/eth-net-intelligence-api" ]]; then
+    	echo "remove $ETHNETPATH before install" 
     	exit 1
 else
-	cd $HOME
+	cd $ETHNETPATH
 	git clone https://github.com/cubedro/eth-net-intelligence-api.git
 	cd  eth-net-intelligence-api
 	npm install

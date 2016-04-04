70698299ad516262fb6ff783b4ec716132aa3868
zekaf <jose.faisca@gmail.com>
2016-04-04 12:49:10 +0100

    Update start_go-ethereum.sh

--- eth-scripts/start_go-ethereum.sh
+++ eth-scripts/start_go-ethereum.sh
@@ -2,18 +2,21 @@
 #
 # Start go-ethereum with Netstat
 #
+# Logging verbosity: 
+# 0-6 (0=silent, 1=error, 2=warn, 3=info, 4=core, 5=debug, 6=debug detail)
+#
 
 LOG=$ETHNETPATH/ethereum.log
 ERR=$ETHNETPATH/ethereum.err
 
 # start  Netstat
 cd $ETHNETPATH/eth-net-intelligence-api
-pm2 start  $ETHNETPATH/eth-net-intelligence-api/app.json
+pm2 start $ETHNETPATH/eth-net-intelligence-api/app.json
 echo "PM2 started"
 
 # start go-ethereum
-nohup $ETHGOPATH/go-ethereum/build/bin/geth --datadir=$ETHDATADIR --rpc --maxpeers "25" \
---verbosity "0" >$LOG 2>$ERR &
+nohup $ETHGOPATH/go-ethereum/build/bin/geth --datadir "$ETHDATADIR" --rpc --maxpeers "25" \
+--verbosity "2" >$LOG 2>$ERR &
 
 echo "Ethereum started"
 

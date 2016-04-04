e282711b2d025063acf580d900299261d3f3a880
zekaf <jose.faisca@gmail.com>
2016-04-04 11:21:21 +0100

    Update update_eth-net-intelligence-api.sh

--- eth-scripts/update_eth-net-intelligence-api.sh
+++ eth-scripts/update_eth-net-intelligence-api.sh
@@ -3,15 +3,18 @@
 # update eth-net-intelligence-api
 
 if [[ -d "$HOME/eth-net-intelligence-api" ]]; then
-	 tmpfile=$(mktemp /tmp/app.json.XXXXXX)
-     cd $HOME/eth-net-intelligence-api
-     cp app.json $tmpfile
-     git reset --hard
-     git pull
-     cp $tmpfile ./
-     rm $tmpfile
+	pm2 stop node-app &&
+	tmpfile=$(mktemp /tmp/app.json.XXXXXX)
+     	cd $HOME/eth-net-intelligence-api
+     	cp app.json $tmpfile
+     	git reset --hard
+     	git pull
+	sudo npm update
+     	cp $tmpfile ./app.json
+     	rm $tmpfile
+     	pm2 gracefulReload node-app
 else
-     echo "$HOME/eth-net-intelligence-api not found" 
+     	echo "$HOME/eth-net-intelligence-api not found" 
 fi
 
 exit 0

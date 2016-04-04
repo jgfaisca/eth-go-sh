e251e8f4ef0951cb6a789727de764f08e21cc6d3
zekaf <jose.faisca@gmail.com>
2016-04-04 12:53:05 +0100

    Update start_go-ethereum.sh

--- eth-scripts/start_go-ethereum.sh
+++ eth-scripts/start_go-ethereum.sh
@@ -6,8 +6,11 @@
 # 0-6 (0=silent, 1=error, 2=warn, 3=info, 4=core, 5=debug, 6=debug detail)
 #
 
-LOG=$ETHNETPATH/ethereum.log
-ERR=$ETHNETPATH/ethereum.err
+LOGPATH=./
+mkdir -p $LOGPATH/log
+
+LOG=$LOGPATH/ethereum.log
+ERR=$LOGPATH/ethereum.err
 
 # start  Netstat
 cd $ETHNETPATH/eth-net-intelligence-api
